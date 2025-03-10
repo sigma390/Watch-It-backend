69e104d94cf15dcf2349be0ecf4c362b5559d1c5
Omkar Patil <git21cs072-t@students.git.edu>
2025-03-10 22:58:03 +0530

    Implemented DB pool , with pgx Driver

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,14 +13,27 @@ const port = 8000
 // type For App Config basiacally it Stores All
 type application struct {
 	Domain string
+	DSN    string
+	DB     *sql.DB
 }
 
 func main() {
 	// app Config
 	var app application
+
+	//read From Command Line (DSN me Store kr rhe hai yeh Data )
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=postgres sslmode=disable connect_timeout=5", "Postgres Connection String")
+	flag.Parse()
+
 	//use of that App variable
 	app.Domain = "localhost"
 	//db conenction
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = conn
+	defer app.DB.Close() //Closing Connection Pool
 
 	//handler
 	//===========> OLD WAY <===================
@@ -30,7 +45,8 @@ func main() {
 	//===============? OLD WAY <=====================
 	// err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	//================> NEW WAY <=======================
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	fmt.Println("Starting Server on port", port)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
--- cmd/api/middlewares.go
+++ cmd/api/middlewares.go
@@ -4,14 +4,19 @@ import "net/http"
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers for all responses
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		// Handle preflight request (OPTIONS method)
 		if r.Method == http.MethodOptions {
-			w.Header().Set("Access-Control-Allow-Origin", "http://*")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.WriteHeader(http.StatusOK)
-		} else {
-			h.ServeHTTP(w, r)
+			return
 		}
+
+		// Call the next handler in the chain
+		h.ServeHTTP(w, r)
 	})
 }
